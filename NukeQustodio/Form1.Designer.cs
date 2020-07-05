551a5547a55d5134312cc2111334bdd6c8063cfb
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-07-05 18:25:19 +1000

    Add files via upload

---
+++ NukeQustodio/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace NukeQustodio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nuke = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nuke
+            // 
+            this.nuke.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nuke.Location = new System.Drawing.Point(147, 199);
+            this.nuke.Name = "nuke";
+            this.nuke.Size = new System.Drawing.Size(111, 42);
+            this.nuke.TabIndex = 0;
+            this.nuke.Text = "NUKE";
+            this.nuke.UseVisualStyleBackColor = true;
+            this.nuke.Click += new System.EventHandler(this.nuke_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(399, 269);
+            this.Controls.Add(this.nuke);
+            this.Name = "Form1";
+            this.Text = "NukeQustodio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nuke;
+    }
+}
+
---
+++ NukeQustodio/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NukeQustodio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nuke_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:/Program Files (x86)/Qustodio");
+            DialogResult dialogResult = MessageBox.Show("By continuing, your parents can find out you deleted Qustodio, proceed with caution.", "WARNING", MessageBoxButtons.OKCancel);
+            if (dialogResult == DialogResult.OK)
+            {
+                MessageBox.Show("Nuke launched");
+                if (di.Exists)
+                {
+                    DeleteDirectory(@"C:/Program Files (x86)/Qustodio");
+                    MessageBox.Show("Nuke has hit the target");
+                }
+                else
+                {
+                    MessageBox.Show("Qustodio was not found on your computer", "Error");
+                }
+            }
+            else if (dialogResult == DialogResult.Cancel)
+            {
+                MessageBox.Show("Cancelled");
+            }
+        }
+
+        private static void DeleteDirectory(string target)
+        {
+            string[] files = Directory.GetFiles(target);
+            string[] dir1 = Directory.GetDirectories(target);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+            foreach (string dir in dir1)
+            {
+                DeleteDirectory(dir);
+            }
+            Directory.Delete(target, true);
+        }
+    }
+}
